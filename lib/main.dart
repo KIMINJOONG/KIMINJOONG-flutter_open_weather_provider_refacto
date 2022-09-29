9673dc243b6f33151eab8f0fa6e120d6f8498046
KimInJoong <ijboym216@gmail.com>
2022-09-29 14:05:15 +0900

    1. ChangeNotifierProvider + ProxyProvider로 refactoring

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_weather_provider_refactor/pages/home_page.dart';
+import 'package:open_weather_provider_refactor/providers/providers.dart';
+import 'package:open_weather_provider_refactor/providers/weather/weather_provider.dart';
+import 'package:open_weather_provider_refactor/repositories/weather_repository.dart';
+import 'package:open_weather_provider_refactor/services/weather_api_services.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<WeatherRepository>(
+          create: (context) => WeatherRepository(
+            weatherApiServices: WeatherApiServices(
+              httpClient: http.Client(),
+            ),
+          ),
+        ),
+        ChangeNotifierProvider<WeatherProvider>(
+          create: (context) => WeatherProvider(
+            weatherRepository: context.read<WeatherRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<TempSettingsProvider>(
+          create: (context) => TempSettingsProvider(),
+        ),
+        ProxyProvider<WeatherProvider, ThemeProvider>(
+          update: (
+            BuildContext context,
+            WeatherProvider weatherProvider,
+            _,
+          ) =>
+              ThemeProvider(wp: weatherProvider),
+        )
+      ],
+      builder: (context, _) => MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.light
+            ? ThemeData.light()
+            : ThemeData.dark(),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,36 @@
+import 'package:open_weather_provider_refactor/exceptions/weather_exception.dart';
+import 'package:open_weather_provider_refactor/models/custom_error.dart';
+import 'package:open_weather_provider_refactor/models/direct_geocoding.dart';
+import 'package:open_weather_provider_refactor/models/weather.dart';
+import 'package:open_weather_provider_refactor/services/weather_api_services.dart';
+
+class WeatherRepository {
+  final WeatherApiServices weatherApiServices;
+
+  WeatherRepository({
+    required this.weatherApiServices,
+  });
+
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiServices.getDirectGeocoding(city);
+
+      final Weather tempWeather =
+          await weatherApiServices.getWeather(directGeocoding);
+
+      final Weather weather = tempWeather.copyWith(
+        name: directGeocoding.name,
+        country: directGeocoding.country,
+      );
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(errMsg: e.message);
+    } catch (e) {
+      print(e);
+      rethrow;
+      // throw CustomError(errMsg: e.toString());
+    }
+  }
+}
